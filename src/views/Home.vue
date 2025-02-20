ed79d8e058c917e892724fcd16330814f262179e
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-02-21 00:12:54 +0900

    update timeline sample

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,92 +16,119 @@
       <!-- Timeline Posts -->
       <div class="space-y-6">
         <!-- Post 1 -->
-        <div class="bg-white shadow-md rounded-2xl p-4">
-          <div class="flex gap-4">
-            <img src="https://via.placeholder.com/50" alt="User Avatar" class="w-12 h-12 rounded-full" />
-            <div class="w-full">
-              <div class="flex justify-between items-center">
-                <div>
-                  <span class="inline-block bg-blue-100 text-blue-600 font-semibold px-2 py-1 rounded-full mt-2 mr-3">
-                    本の感想
-                  </span>
-                  <span class="font-bold text-blue-600">山田太郎</span>
-                  <span class="text-gray-500 text-sm ml-2">@taro_yamada</span>
-                </div>
-                <span class="text-gray-400 text-sm">1時間前</span>
-              </div>
-              <p class="text-gray-800 mt-2">ITエンジニアのための機械学習理論入門</p>
+        <router-link
+          :to="`/post/${1}`"
+          class="hover:bg-gray-100 transition"
+        >
+          <div class="bg-white shadow-md rounded-2xl p-4 my-6">
+            <div class="flex gap-4">
               <img
-              src="https://via.placeholder.com/400"
-              alt="ここに本の画像"
-              class="w-full rounded-lg shadow-md mt-3"
+                src="https://via.placeholder.com/50"
+                alt="User Avatar"
+                class="w-12 h-12 rounded-full"
               />
-              <div class="flex justify-between text-gray-500 mt-4 text-sm">
-                <span class="cursor-pointer hover:text-blue-600">コメント 12</span>
-                <span class="cursor-pointer hover:text-blue-600">いいね 89</span>
+              <div class="w-full">
+                <div class="flex justify-between items-center">
+                  <div>
+                    <span class="inline-block bg-blue-100 text-blue-600 font-semibold px-2 py-1 rounded-full mt-2 mr-3">
+                      本の感想
+                    </span>
+                    <span class="font-bold text-blue-600">山田太郎</span>
+                    <span class="text-gray-500 text-sm ml-2">@taro_yamada</span>
+                  </div>
+                  <span class="text-gray-400 text-sm">1時間前</span>
+                </div>
+                <p class="text-gray-800 mt-2">ITエンジニアのための機械学習理論入門</p>
+                <img
+                  src="http://books.google.com/books/content?id=i1eYjgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
+                  alt=""
+                  class=" rounded-lg shadow-md mt-3"
+                />
+                <div class="flex text-gray-500 mt-4 text-sm">
+                  <span class="cursor-pointer mr-3">コメント 12</span>
+                  <span class="cursor-pointer">いいね 89</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </router-link>
 
         <!-- Post 2 -->
-        <div class="bg-white shadow-md rounded-2xl p-4">
-          <div class="flex gap-4">
-            <img src="https://via.placeholder.com/50" alt="User Avatar" class="w-12 h-12 rounded-full" />
-            <div class="w-full">
-              <div class="flex justify-between items-center">
-                <div>
-                  <span class="inline-block bg-blue-100 text-blue-600 font-semibold px-2 py-1 rounded-full mt-2 mr-3">
-                    本の感想
-                  </span>
-                  <span class="font-bold text-blue-600">山田太郎</span>
-                  <span class="text-gray-500 text-sm ml-2">@taro_yamada</span>
-                </div>
-                <span class="text-gray-400 text-sm">1時間前</span>
-              </div>
-              <p class="text-gray-800 mt-2">ITエンジニアのための機械学習理論入門</p>
+        <router-link
+          :to="`/post/${1}`"
+          class="hover:bg-gray-100 transition"
+        >
+          <div class="bg-white shadow-md rounded-2xl p-4 my-6">
+            <div class="flex gap-4">
               <img
-              src="https://via.placeholder.com/400"
-              alt="ここに本の画像"
-              class="w-full rounded-lg shadow-md mt-3"
+                src="https://via.placeholder.com/50"
+                alt="User Avatar"
+                class="w-12 h-12 rounded-full"
               />
-              <div class="flex justify-between text-gray-500 mt-4 text-sm">
-                <span class="cursor-pointer hover:text-blue-600">コメント 12</span>
-                <span class="cursor-pointer hover:text-blue-600">いいね 89</span>
+              <div class="w-full">
+                <div class="flex justify-between items-center">
+                  <div>
+                    <span class="inline-block bg-blue-100 text-blue-600 font-semibold px-2 py-1 rounded-full mt-2 mr-3">
+                      本の感想
+                    </span>
+                    <span class="font-bold text-blue-600">山田太郎</span>
+                    <span class="text-gray-500 text-sm ml-2">@taro_yamada</span>
+                  </div>
+                  <span class="text-gray-400 text-sm">1時間前</span>
+                </div>
+                <p class="text-gray-800 mt-2">ITエンジニアのための機械学習理論入門</p>
+                <img
+                  src="http://books.google.com/books/content?id=i1eYjgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
+                  alt=""
+                  class=" rounded-lg shadow-md mt-3"
+                />
+                <div class="flex text-gray-500 mt-4 text-sm">
+                  <span class="cursor-pointer mr-3">コメント 12</span>
+                  <span class="cursor-pointer">いいね 89</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </router-link>
+
 
         <!-- Post 3 -->
-        <div class="bg-white shadow-md rounded-2xl p-4">
-          <div class="flex gap-4">
-            <img src="https://via.placeholder.com/50" alt="User Avatar" class="w-12 h-12 rounded-full" />
-            <div class="w-full">
-              <div class="flex justify-between items-center">
-                <div>
-                  <span class="inline-block bg-blue-100 text-blue-600 font-semibold px-2 py-1 rounded-full mt-2 mr-3">
-                    本の感想
-                  </span>
-                  <span class="font-bold text-blue-600">山田太郎</span>
-                  <span class="text-gray-500 text-sm ml-2">@taro_yamada</span>
-                </div>
-                <span class="text-gray-400 text-sm">1時間前</span>
-              </div>
-              <p class="text-gray-800 mt-2">ITエンジニアのための機械学習理論入門</p>
+        <router-link
+          :to="`/post/${1}`"
+          class="hover:bg-gray-100 transition"
+        >
+          <div class="bg-white shadow-md rounded-2xl p-4 my-6">
+            <div class="flex gap-4">
               <img
-              src="https://via.placeholder.com/400"
-              alt="ここに本の画像"
-              class="w-full rounded-lg shadow-md mt-3"
+                src="https://via.placeholder.com/50"
+                alt="User Avatar"
+                class="w-12 h-12 rounded-full"
               />
-              <div class="flex justify-between text-gray-500 mt-4 text-sm">
-                <span class="cursor-pointer hover:text-blue-600">コメント 12</span>
-                <span class="cursor-pointer hover:text-blue-600">いいね 89</span>
+              <div class="w-full">
+                <div class="flex justify-between items-center">
+                  <div>
+                    <span class="inline-block bg-blue-100 text-blue-600 font-semibold px-2 py-1 rounded-full mt-2 mr-3">
+                      本の感想
+                    </span>
+                    <span class="font-bold text-blue-600">山田太郎</span>
+                    <span class="text-gray-500 text-sm ml-2">@taro_yamada</span>
+                  </div>
+                  <span class="text-gray-400 text-sm">1時間前</span>
+                </div>
+                <p class="text-gray-800 mt-2">ITエンジニアのための機械学習理論入門</p>
+                <img
+                  src="http://books.google.com/books/content?id=i1eYjgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
+                  alt=""
+                  class=" rounded-lg shadow-md mt-3"
+                />
+                <div class="flex text-gray-500 mt-4 text-sm">
+                  <span class="cursor-pointer mr-3">コメント 12</span>
+                  <span class="cursor-pointer">いいね 89</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
+        </router-link>
 
 
 
