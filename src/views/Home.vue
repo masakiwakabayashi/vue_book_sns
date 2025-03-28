99337c792c20c36b4d0948eac4b6a5db1489d49f
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-03-28 21:03:28 +0900

    test view image in supabase storage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,34 @@
+<script setup>
+import { supabase } from '@/lib/supabaseClient';
+import { onMounted, ref } from 'vue';
+
+const imageUrl = ref(null);
+
+// TODO: これはあとで別ファイルに移す
+const getPublicUrl = (bucket, path) => {
+  const { data } = supabase.storage.from(bucket).getPublicUrl(path)
+  return data.publicUrl
+}
+
+onMounted(() => {
+  // 例: 'images' バケットに 'my-folder/my-image.jpg' がある場合
+  imageUrl.value = getPublicUrl('test', '1.png')
+})
+
+</script>
+
+
 <template>
   <div class="bg-blue-50 min-h-screen p-6">
     <div class="max-w-2xl mx-auto">
       <!-- Header -->
       <div class="bg-white shadow-md rounded-2xl p-6 mb-6">
         <h1 class="text-blue-600 text-3xl font-bold">タイムライン</h1>
+      </div>
+
+      <!-- 画像の表示テスト -->
+      <div>
+        <img v-if="imageUrl" :src="imageUrl" alt="My test Image" />
       </div>
 
       <!-- Post Input Section -->
